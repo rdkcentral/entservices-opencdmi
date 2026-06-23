2e4c578f971c4368ada0305e0cabbc9df05274e0
LekshmiMR <mohan_lekshmi@comcast.com>
2026-06-23 19:52:37 +0000

    Update the thunder and thunderclientlib to latest version to fix native build error

--- build_dependencies.sh
+++ build_dependencies.sh
@@ -47,9 +47,9 @@ cd ..
 
 git clone --branch  R4.4.3 https://github.com/rdkcentral/ThunderTools.git
 
-git clone --branch R4.4.1 https://github.com/rdkcentral/Thunder.git
+git clone --branch R4.4.5 https://github.com/rdkcentral/Thunder.git
 
-git clone --branch R4.4.1 https://github.com/rdkcentral/ThunderClientLibraries.git
+git clone --branch R4.4.2 https://github.com/rdkcentral/ThunderClientLibraries.git
 
 git clone --branch main https://github.com/rdkcentral/entservices-apis.git
 
